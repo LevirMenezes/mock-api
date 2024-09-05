6ab2cc231d0903add45582aa95d6617b6c2ebdf8
Levir Menezes <levir.menezes@redeig.com>
2024-09-05 14:56:15 -0400

    adiciona busca de despesas por mes

--- controllers/deputado_controller.js
+++ controllers/deputado_controller.js
@@ -11,29 +11,37 @@ export class DeputadoController {
   }
 
   static obterDeputados(_req, res) {
+    try {
+      const deputados = JSON.parse(DeputadoController.lerArquivoJson());
 
-    const deputados = JSON.parse(DeputadoController.lerArquivoJson());
+      // Verifica se há resultados e responde de acordo
+      if (deputados.length === 0) {
+        return res.status(404).send('Nenhum deputado encontrado!');
+      } else {
+        return res.json({ ...deputados });
+      }
+    } catch (error) {
+      console.error('Erro ao obter lista de deputados:', error.message); // Log do erro ao obter despesas
+      return res.status(500).send('Erro interno do servidor'); // Retorna status code 500
+    }
 
-    if (!deputados)
-      res.status(500).send('Erro ao ler o arquivo JSON');
-    else
-      res.json({ ...deputados });
-  }  
+
+  }
 
   static obterDeputado(req, res) {
     try {
       const deputados = JSON.parse(DeputadoController.lerArquivoJson()); // Lê o arquivo JSON com os deputados
-  
+
       // Obtém os parâmetros 'nome' e 'partido' da query string, usando string vazia como valor padrão se não estiver definido
-      const nome = req.query.nome || '';     
-      const partido = req.query.partido || ''; 
-  
+      const nome = req.query.nome || '';
+      const partido = req.query.partido || '';
+
       // Filtra os deputados com base nos parâmetros da query string
-      const resultado = deputados.deputados.filter(deputado => 
+      const resultado = deputados.deputados.filter(deputado =>
         deputado.nome.toLowerCase().includes(nome.toLowerCase()) &&
         deputado.siglaPartido.toLowerCase().includes(partido.toLowerCase())
       );
-  
+
       // Verifica se há resultados e responde de acordo
       if (resultado.length === 0) {
         return res.status(404).send('Nenhum deputado encontrado com os critérios fornecidos.');
